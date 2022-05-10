b8cd6dd836f67c2cca4bf641e3eb509b50892e0f
BabulKumar2022 <bb3583400@gmail.com>
2022-05-10 08:34:19 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Contact from './Pages/Contact/Contact';
+import Home from './Pages/Home/Home/Home';
 import Navbar from './Pages/Home/Shared/Navbar';
+import Login from './Pages/Login/Login';
+import Review from './Pages/Review/Review';
+import Appointment from './Pages/Appointment/Appointment';
+import About from './Pages/About/About';
 
 function App() {
   return (
     <div className="">
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/review' element={<Review></Review>}></Route>
+        <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+
+      </Routes>
     </div>
   );
 }
