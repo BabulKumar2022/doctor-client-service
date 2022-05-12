4f628e0fa3415ebaeea956d70c64b611be1baa54
BabulKumar2022 <bb3583400@gmail.com>
2022-05-13 01:25:10 +0600

    modal

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -4,8 +4,8 @@ import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import { format } from "date-fns";
 
-const AppointmentBanner = () => {
-    const [date, setDate] = useState(new Date());
+const AppointmentBanner = ({date, setDate}) => {
+ 
   return (
     <div class="hero min-h-screen ">
       <div class="hero-content flex-col lg:flex-row-reverse">
@@ -16,7 +16,7 @@ const AppointmentBanner = () => {
            selected={date}
            onSelect={setDate}
            />
-           <p>You have selected: {format(date, 'pp')}</p>
+           
         </div>
       </div>
     </div>
---
+++ src/Pages/Appointment/BookingModal.js
@@ -0,0 +1,58 @@
+import { format } from "date-fns";
+import React from "react";
+
+const BookingModal = ({ date, treatment }) => {
+  const { name, slots } = treatment;
+  return (
+    <div>
+      <input type="checkbox" id="booking-modal" class="modal-toggle" />
+      <div class="modal modal-bottom sm:modal-middle">
+        <div class="modal-box">
+          <label
+            for="booking-modal"
+            class="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 class="font-bold text-lg text-secondary">Booking for:{name}</h3>
+          <form className=" grid  grid-cols-1 gap-5 ">
+            <input
+              type="text" disabled
+               value={format(date, "PP")}
+              
+              class="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="text"
+              placeholder="Type here"
+              class="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="text"
+              placeholder="Type here"
+              class="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="text"
+              placeholder="Type here"
+              class="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="text"
+              placeholder="Type here"
+              class="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="submit"
+              placeholder="Type here"
+              class="btn btn-secondary w-full max-w-xs"
+            />
+          </form>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookingModal;
